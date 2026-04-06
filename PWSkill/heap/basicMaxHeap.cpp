63d133ef3ba3cec8ad4d793cbcda7bd5912e5ed3
Abdul Qadir <qadir21@users.noreply.github.com>
2026-04-06 17:51:56 +0530

    fix: resolve deleted files

--- PWSkill/heap/basicMaxHeap.cpp
+++ PWSkill/heap/basicMaxHeap.cpp
@@ -8,5 +8,21 @@ int main(){
     pq.push(-6);
     pq.push(8);
     cout << pq.top() << endl;
+    pq.push(81);
+    cout << pq.top() << endl;
+    pq.pop();
+    cout << pq.top() << endl;
+    cout << endl;
+
+    priority_queue<int, vector<int>, greater<int>> p;
+    p.push(10);
+    p.push(2);
+    p.push(-6);
+    p.push(8);
+    cout << p.top() << endl;
+    p.push(-81);
+    cout << p.top() << endl;
+    p.pop();
+    cout << p.top() << endl;
     return 0;
 }
